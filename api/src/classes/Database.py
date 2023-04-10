95fe4075ccaae6e902582b2d10e6a8d57cb73003
AdamS <adams.manchester2019a@gmail.com>
2023-04-10 20:44:00 +0100

    Added api code style tests

--- api/src/classes/Database.py
+++ api/src/classes/Database.py
@@ -4,19 +4,23 @@ from flask import abort
 import os
 from dotenv import load_dotenv
 import time
+
 load_dotenv()
+
+
 class Database:
     connection = None
     cursor = None
     connection_attemps = 0
 
     def __init__(self):
-        if(os.getenv('MODE') == "test"):
+        if (os.getenv('MODE') == "test"):
             print("Testing mode - Will not connect to database")
         else:
-            if(self.connection_attemps<6):
+            if (self.connection_attemps < 6):
                 self.connection_attemps += 1
-                print("Connecting to SQL Server. Attempt:", self.connection_attemps)
+                print("Connecting to SQL Server. Attempt:",
+                      self.connection_attemps)
                 try:
                     self.connection = connect(
                         host=os.getenv('DB_ADDRESS'),
@@ -29,77 +33,79 @@ class Database:
                         record = self.cursor.fetchone()
                         print("Connected to", record)
                 except:
-                    print("Connection error, waiting:", (self.connection_attemps+1)**2, " seconds")
-                    time.sleep((self.connection_attemps+1)**2)
+                    print("Connection error, waiting:",
+                          (self.connection_attemps + 1)**2, " seconds")
+                    time.sleep((self.connection_attemps + 1)**2)
                     self.__init__()
             else:
                 return abort(500, "Could not connect to SQL server")
 
     def check_connection(self):
-        if(not self.connection.is_connected()):
+        if (not self.connection.is_connected()):
             self.__init__()
 
     #Insert single ticket
-    def insert_into_tickets(self, title,description,status,user_id):
+    def insert_into_tickets(self, title, description, status, user_id):
         self.check_connection()
         insert_ticket_query = "INSERT INTO Tickets (title, description, status, user_id) VALUES (%s,%s,%s,%s)"
-        self.cursor.execute(insert_ticket_query, (title, description, status, user_id))
+        self.cursor.execute(insert_ticket_query,
+                            (title, description, status, user_id))
         self.connection.commit()
 
     #Insert single user
-    def insert_into_users(self, email,password,type):
+    def insert_into_users(self, email, password, type):
         self.check_connection()
         insert_user_query = "INSERT INTO Users (email, password,type) VALUES (%s,%s,%s)"
-        self.cursor.execute(insert_user_query, (email,password,type))
+        self.cursor.execute(insert_user_query, (email, password, type))
         self.connection.commit()
 
     #Insert single comment
-    def insert_into_comments(self, text,ticket_id,user_id):
+    def insert_into_comments(self, text, ticket_id, user_id):
         self.check_connection()
         insert_comment_query = "INSERT INTO Comments (text, ticket_id, user_id) VALUES (%s,%s,%s)"
-        self.cursor.execute(insert_comment_query, (text,ticket_id,user_id))
+        self.cursor.execute(insert_comment_query, (text, ticket_id, user_id))
         self.connection.commit()
 
     #Get single user with email
     def get_user_with_email(self, email):
         self.check_connection()
         select_customers_query = "SELECT * FROM Users WHERE email = %s"
-        self.cursor.execute(select_customers_query, (email,))
+        self.cursor.execute(select_customers_query, (email, ))
         return self.parse_result(self.cursor)[0]
-    
+
     #Get all users with email
     def get_users_with_email(self, email):
         self.check_connection()
         select_customers_query = "SELECT * FROM Users WHERE email = %s"
-        self.cursor.execute(select_customers_query, (email,))
+        self.cursor.execute(select_customers_query, (email, ))
         return self.parse_result(self.cursor)
-    
+
     #Get all nromal users
     def get_normal_users(self):
         self.check_connection()
         select_customers_query = "SELECT id, email, signupDate FROM Users WHERE type='User'"
         self.cursor.execute(select_customers_query)
         return self.parse_result(self.cursor)
-    
+
     #Get all users
     def get_users(self):
         self.check_connection()
         select_users_query = "SELECT id, email, signupDate FROM Users"
         self.cursor.execute(select_users_query)
         return self.parse_result(self.cursor)
-    
+
     #Get specific ticket
     def get_ticket_with_id(self, id):
         self.check_connection()
         select_ticket_query = "SELECT * FROM Tickets WHERE id = %s"
-        self.cursor.execute(select_ticket_query, (id,))
+        self.cursor.execute(select_ticket_query, (id, ))
         return self.parse_result(self.cursor)[0]
 
     #Update specific ticket status
-    def update_status_of_ticket(self, id,status):
+    def update_status_of_ticket(self, id, status):
         self.check_connection()
         update_ticket_query = "UPDATE Tickets SET status=%s WHERE id = %s"
-        self.cursor.execute(update_ticket_query, (status,id))
+        self.cursor.execute(update_ticket_query, (status, id))
 
     #Get all tickets
     def get_all_tickets(self):
@@ -117,10 +123,9 @@ class Database:
 
     # Database utils
     def parse_result(self, cur):
-        row_headers=[x[0] for x in cur.description]
+        row_headers = [x[0] for x in cur.description]
         rv = cur.fetchall()
-        json_data=[]
+        json_data = []
         for result in rv:
-             json_data.append(dict(zip(row_headers,result)))
+            json_data.append(dict(zip(row_headers, result)))
         return json_data
-        
--- api/tests/test_classes.py
+++ api/tests/test_classes.py
@@ -1,27 +1,29 @@
 from src.classes.User import User
 from src.classes.Comment import Comment
 
+
 def test_user_class():
     email = "example@example.com"
     passwordHash = "1234"
     type = "admin"
-    user = User(email,passwordHash,type)
+    user = User(email, passwordHash, type)
 
     assert user.get_email() is email
     assert user.get_passwordHash() is passwordHash
     assert user.get_type() is type
 
+
 def test_Comment_class():
     email = "example@example.com"
     passwordHash = "1234"
     type = "admin"
-    user = User(email,passwordHash,type)
+    user = User(email, passwordHash, type)
     user.set_id(1)
 
     text = "comment text"
     ticket_id = 45323
-    commet = Comment(text,ticket_id,user)
+    commet = Comment(text, ticket_id, user)
 
     assert commet.get_text() is text
-    assert commet.get_user_id() is 1
-    assert commet.get_ticket_id() is 45323
+    assert commet.get_user_id() == 1
+    assert commet.get_ticket_id() == 45323
